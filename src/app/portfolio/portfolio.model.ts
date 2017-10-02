7306e7cc5eecb1bd39d16652e042d3946e93167b
Nicholas  Vorraso <nvorraso2020@gmail.com>
2017-10-01 22:12:10 -0400

    Minor website style improvements

--- src/app/portfolio/portfolio.model.ts
+++ src/app/portfolio/portfolio.model.ts
@@ -4,14 +4,14 @@ export class ProjectModel {
     {
         id: "project_0",
         href: "https://shorten2020.herokuapp.com/",
-        imgSrc: "https://image.ibb.co/dNLNEv/url_shortener_ss.png",
+        imgSrc: "../assets/shorten.png",
         name: "Express.Js URL Shortener",
         description: "A URL shortener demo using Express.Js and MongoDB."
     },
     {
         id: "project_1",
       //   href: "https://codepen.io/nickey_vee/pen/JKgLwV",
-        imgSrc: "https://image.ibb.co/i66mUa/Screenshot_31.png",
+        imgSrc: "../assets/wiki.jpg",
         name: "WikiPedia Viewer",
         description: `A Wikipedia-Search UI. Utilizes Wikipedia's open source search api using ajax.`,
         route: "/wiki-viewer"
@@ -19,7 +19,7 @@ export class ProjectModel {
     {
         id: "project_2",
       //   href: "https://codepen.io/nickey_vee/pen/VPKpNe",
-        imgSrc: "http://image.ibb.co/c4QYWF/Screenshot_2017_01_23_at_7_37_44_PM.png",
+        imgSrc: "../assets/simon.jpg",
         name: "Simon Game",
         description: `Clone of the famous 80's memory game, as authentic as the real thing!`,
         route: "/simon-game"
@@ -28,7 +28,7 @@ export class ProjectModel {
     {
         id: "project_3",
       //   href: "https://codepen.io/nickey_vee/pen/dOPNJx",
-        imgSrc: "http://image.ibb.co/bHaBJv/Screenshot_2017_01_25_at_8_02_57_PM.png",
+        imgSrc: "../assets/calc.jpg",
         name: "Javascript Calculator",
         description: `A standard-function calculator for Free Code Camp. Built with JavaScript and jQuery.`,
         route: "/calculator"
@@ -36,7 +36,7 @@ export class ProjectModel {
     {
         id: "project_4",
       //   href: "https://codepen.io/nickey_vee/pen/LxEogK",
-        imgSrc: "http://image.ibb.co/ipL0BF/Screenshot_2017_01_25_at_8_05_48_PM.png",
+        imgSrc: "../assets/tic-tac-toe.jpg",
         name: "unbeatable tic-tac-toe",
         description: `A game of Tic-Tac-Toe with an AI opponent. You can only tie!`,
         route: "/tic-tac-toe"
@@ -44,7 +44,7 @@ export class ProjectModel {
     {
         id: "project_5",
       //   href: "https://codepen.io/nickey_vee/pen/YWBvpX",
-        imgSrc: "http://image.ibb.co/haFYWF/Screenshot_2017_01_23_at_7_57_24_PM.png",
+        imgSrc: "../assets/quote.jpg",
         name: "Quote Generator",
         description: `Random quotes by the greatest people of history. One of my first projects.`,
         route: "/quote-gen"
@@ -52,9 +52,16 @@ export class ProjectModel {
     {
         id: "project_6",
       //   href: "https://codepen.io/nickey_vee/pen/JEdaqM" ,
-        imgSrc: "http://image.ibb.co/fB9Gka/Screenshot_2017_01_25_at_7_59_34_PM.png",
+        imgSrc: "../assets/pomo.jpg",
         name: "Pomodoro Clock",
         description: `A Pomodoro timer. I built this one to practice my JavaScript Skills.`,
         route: "/pomodoro"
     },
+    {
+       id: "project_7",
+       href: "https://razz-interactive-project.herokuapp.com/",
+       imgSrc: "../assets/razz.jpg",
+       name: "VueJs UI",
+       description: "A simple User Interface I made for a company application using Vue.js & Express.js",
+    }
 ]}
--- src/app/router.animations.ts
+++ src/app/router.animations.ts
@@ -17,4 +17,4 @@ export const routerTransition = trigger('routerTransition', [
       ], { optional: true }),
     ])
   ])
-])
+]);
